102c61784b5378807eb1e2ad20f4f90beeca1772
Abdi-01 <abdialghi@gmail.com>
2021-10-27 21:42:36 +0700

    first commit

--- src/Navigations/TabNavigation.js
+++ src/Navigations/TabNavigation.js
@@ -2,14 +2,31 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomePage from '../Pages/Home';
 import CartPage from '../Pages/Cart';
+import { Icon } from 'react-native-elements';
 
 const Tab = createBottomTabNavigator()
+
 const TabNavigation = (props) => {
 
     return (
-        <Tab.Navigator initialRouteName="Home">
+        <Tab.Navigator initialRouteName="Home"
+            screenOptions={
+                ({ route }) => ({
+                    tabBarIcon: ({ color }) => {
+                        let iconName;
+                        if (route.name == "Home") {
+                            iconName = "home";
+                        } else if (route.name == "Cart") {
+                            iconName = "shopping-bag";
+                        }
+
+                        return <Icon name={iconName} size={24} color={color} />
+                    }
+                })
+            }
+        >
             <Tab.Screen name="Home" component={HomePage} />
-            <Tab.Screen name="Cart" component={CartPage} options={{headerShown:false}}/>
+            <Tab.Screen name="Cart" component={CartPage} options={{ headerShown: false }} />
         </Tab.Navigator>
     )
 }
