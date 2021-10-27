194f302400658ae763f8535fc679056ac282abb0
Abdi-01 <abdialghi@gmail.com>
2021-10-27 19:33:31 +0700

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigation from './src/Navigations/StackNavigation';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import Reducers from './src/redux/reducers';
+import ReduxThunk from 'redux-thunk';
+
+const globalStore = createStore(Reducers, {}, applyMiddleware(ReduxThunk));
 
 const App = () => {
 
   return (
-    <NavigationContainer>
-      <StackNavigation />
-    </NavigationContainer>
+    <Provider store={globalStore}>
+      <NavigationContainer>
+        <StackNavigation />
+      </NavigationContainer>
+    </Provider>
   )
 }
 
