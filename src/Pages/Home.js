e2ba969cadb0127c3dd92ac2634648efdfe68a4f
Abdi-01 <abdialghi@gmail.com>
2021-10-28 20:38:42 +0700

    first commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,17 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View } from 'react-native'
-import { Button, Header, Icon, SearchBar, Text } from "react-native-elements"
+import React, { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, View } from 'react-native'
+import { Button, Header, Icon, Image, SearchBar, Text } from "react-native-elements"
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProductsAction } from '../redux/actions';
 
 const HomePage = (props) => {
+
+    const [banner, setBanner] = useState([
+        "https://cdn-2.tstatic.net/tribunnews/foto/bank/images/3-pengorbanan-gokil-masyarakat-manfaatkan-promo-e-commerce.jpg",
+        "https://asset-a.grid.id/crop/0x0:0x0/700x0/photo/2019/09/09/4070068564.jpeg",
+        "https://statik.tempo.co/data/2018/05/14/id_705289/705289_720.jpg"
+    ])
 
     const dispatch = useDispatch()
 
@@ -37,17 +43,30 @@ const HomePage = (props) => {
                 rightComponent={
                     <View style={{ flexDirection: "row", flex: 1, alignItems: "center" }}>
                         <Icon name="heart" type="feather" size={22}
-                            iconStyle={{ color: "white" }} style={{marginHorizontal:wp(2)}}
+                            iconStyle={{ color: "white" }} style={{ marginHorizontal: wp(2) }}
                         />
                         <Icon name="bell" type="feather" size={22}
-                            iconStyle={{ color: "white" }} style={{marginHorizontal:wp(2)}}
+                            iconStyle={{ color: "white" }} style={{ marginHorizontal: wp(2) }}
                         />
                         <Icon name="message-circle" type="feather" size={22}
-                            iconStyle={{ color: "white" }} style={{marginHorizontal:wp(2)}}
+                            iconStyle={{ color: "white" }} style={{ marginHorizontal: wp(2) }}
                         />
                     </View>
                 }
             />
+            <View>
+                <FlatList
+                    data={banner}
+                    renderItem={
+                        ({ item }) => (
+                            <Image source={{ uri: item }} style={{ width: wp(90), height: hp(25) }} />
+                        )
+                    }
+                    keyExtractor={(item, index) => index.toString()}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                />
+            </View>
             <Text h2>Home Page</Text>
 
         </View>
