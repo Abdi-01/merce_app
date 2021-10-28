b4ae68ea6bfe04b340e0a82b4fd51acb705e6e78
Abdi-01 <abdialghi@gmail.com>
2021-10-28 19:43:39 +0700

    first commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View } from 'react-native'
 import { Button, Text } from "react-native-elements"
+import { useDispatch } from 'react-redux';
+import { getProductsAction } from '../redux/actions';
 
 const HomePage = (props) => {
+
+    const dispatch = useDispatch()
 
     const btBack = () => {
         props.navigation.goBack()
     }
+
+    useEffect(() => {
+        dispatch(getProductsAction())
+    }, [])
+
     return (
         <View>
             <Text h2>Home Page</Text>
