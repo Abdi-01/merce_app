a6061f504514703a2839897e882abbdd180ae9c0
Abdi-01 <abdialghi@gmail.com>
2021-11-03 19:39:01 +0700

    first commit

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { FlatList, Image, View } from 'react-native'
 import { Button, Card, Text, Icon } from "react-native-elements"
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateCartAction } from '../redux/actions/userAction';
 
 const CartPage = (props) => {
+
+    const dispatch = useDispatch()
 
     const { cartUser, iduser } = useSelector((state) => {
         return {
@@ -13,13 +16,36 @@ const CartPage = (props) => {
         }
     })
 
+    const totalPayment = () => {
+        let total = 0;
+        if (cartUser.length > 0) {
+            cartUser.forEach(item => {
+                total += item.subTotal
+            })
+        }
+
+        return total
+    }
+
+    const btInc = (idx) => {
+        let temp = [...cartUser]
+        temp[idx].qty += 1
+        dispatch(updateCartAction(temp, iduser))
+    }
+    
+    const btDec = (idx) => {
+        let temp = [...cartUser]
+        temp[idx].qty -= 1
+        dispatch(updateCartAction(temp, iduser))
+    }
+
     return (
         <View style={{ flex: 1, backgroundColor: "white", paddingTop: hp(10) }}>
             <FlatList
                 data={cartUser}
                 renderItem={({ item, index }) => (
                     <Card containerStyle={{ width: wp(100), margin: 0 }}>
-                        <View style={{ flexDirection: "row", justifyContent:"space-around" }}>
+                        <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
                             <Image source={{ uri: item.image }} style={{ width: wp(12), height: hp(5) }} />
                             <View>
                                 <Text>{item.nama}</Text>
@@ -28,11 +54,11 @@ const CartPage = (props) => {
                             <View style={{ flexDirection: "row" }}>
                                 <Button type="outline" icon={
                                     <Icon type="feather" name="minus" size={15} />
-                                } />
+                                } onPress={() => btDec(index)} />
                                 <Text h4 style={{ marginHorizontal: 10 }}>{item.qty}</Text>
                                 <Button type="outline" icon={
                                     <Icon type="feather" name="plus" size={15} />
-                                } />
+                                } onPress={() => btInc(index)} />
                             </View>
                         </View>
                     </Card>
@@ -40,6 +66,15 @@ const CartPage = (props) => {
                 keyExtractor={(item, index) => index.toString()}
                 style={{ marginTop: wp(2) }}
             />
+            <View style={{ padding: wp(3), flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+                <View>
+                    <Text>Total Payment</Text>
+                    <Text h4>Rp. {totalPayment()}</Text>
+                </View>
+                <Button
+                    title="Checkout"
+                />
+            </View>
         </View>
     )
 }
