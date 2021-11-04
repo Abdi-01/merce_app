3847f157258ab7f977ee749aca79685a0ffa273f
Abdi-01 <abdialghi@gmail.com>
2021-11-04 19:56:52 +0700

    first commit

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { View } from 'react-native'
-import { Text } from 'react-native-elements';
+import { ImageBackground, View } from 'react-native'
+import { Avatar, Text } from 'react-native-elements';
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
 const AccountPage = (props) => {
 
     return (
-        <View style={{ flex: 1, backgroundColor: "white", paddingTop: hp(10) }}>
-            <Text>Acoount Page</Text>
+        <View style={{ flex: 1, backgroundColor: "white", paddingTop: hp(7) }}>
+            <ImageBackground source={{ uri: "https://img4.goodfon.com/wallpaper/nbig/a/45/ralli-moto-dakar-dakar-sport-rally-skorost-pesok-gonshchik-m.jpg" }}
+                style={{ width: wp(100), height: hp(40) }}
+            >
+                <View style={{ backgroundColor: "rgba(0,0,0,0.5)", height: "100%", justifyContent: "center" }}>
+                    <Avatar
+                        containerStyle={{ alignSelf: "center" }}
+                        source={{ uri: "https://api.duniagames.co.id/api/content/upload/file/8143860661599124172.jpg" }}
+                        size={120}
+                        rounded
+                    >
+                        <Avatar.Accessory
+                            name="edit"
+                            size={35}
+                            iconStyle={{ fontSize: 20 }}
+                        />
+                    </Avatar>
+                    <Text h3 style={{ color: "white", textAlign:"center" }}>Eduardo</Text>
+                </View>
+            </ImageBackground>
         </View>
     )
 };
