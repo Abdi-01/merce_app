3384b0d425f73de411d0b3e6995130356bed378d
Abdi-01 <abdialghi@gmail.com>
2021-11-08 21:09:34 +0700

    first commit

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -14,6 +14,8 @@ const LoginPage = (props) => {
     const [username, setUsername] = useState("")
     const [password, setPass] = useState("")
 
+    const [loginForm, setLoginForm] = useState(false)
+
     // useSelctor : pengganti mapToProps pada class component
     const { iduser } = useSelector((state) => {
         return {
@@ -32,6 +34,8 @@ const LoginPage = (props) => {
             if (check) {
                 let { username, password } = JSON.parse(check)
                 dispatch(loginAction(username, password))
+            } else {
+                setLoginForm(!loginForm)
             }
         } catch (error) {
             console.log(error)
@@ -53,60 +57,76 @@ const LoginPage = (props) => {
         dispatch(loginAction(username, password))
     }
 
+    if (loginForm) {
+        return (
+            <View style={style.container}>
+                <Image source={{ uri: "https://www.sipayo.com/wp-content/uploads/2017/12/e-commerce.png" }}
+                    style={{ width: wp("40%"), height: hp("22%") }}
+                />
+                <View >
+                    <Input
+                        placeholder="username"
+                        inputStyle={{ backgroundColor: "white" }}
+                        leftIcon={
+                            <Icon
+                                type="feather"
+                                name="user"
+                            />
+                        }
+                        inputContainerStyle={{ backgroundColor: "white" }}
+                        containerStyle={{ width: wp("70%") }}
+                        onChangeText={val => setUsername(val)}
+                    />
+                    <Input
+                        placeholder="password"
+                        inputStyle={{ backgroundColor: "white" }}
+                        leftIcon={
+                            <Icon
+                                type="feather"
+                                name="lock"
+                            />
+                        }
+                        inputContainerStyle={{ backgroundColor: "white" }}
+                        containerStyle={{ width: wp("70%") }}
+                        secureTextEntry
+                        onChangeText={val => setPass(val)}
+                    />
+                    <Button
+                        title="Sign In"
+                        containerStyle={{ width: wp("60%"), alignSelf: "center" }}
+                        onPress={btLogin}
+                    />
+                </View>
+                <Text style={style.textA}>Not have account ?
+                    <Text style={style.textRegis}
+                        onPress={btToRegis}
+                    >Register Now</Text>
+                </Text>
+            </View>
+        )
+    }
+
+    // Splashscreen
     return (
-        <View style={style.container}>
+        <View style={style.splashScreen}>
             <Image source={{ uri: "https://www.sipayo.com/wp-content/uploads/2017/12/e-commerce.png" }}
                 style={{ width: wp("40%"), height: hp("22%") }}
             />
-            <View >
-                <Input
-                    placeholder="username"
-                    inputStyle={{ backgroundColor: "white" }}
-                    leftIcon={
-                        <Icon
-                            type="feather"
-                            name="user"
-                        />
-                    }
-                    inputContainerStyle={{ backgroundColor: "white" }}
-                    containerStyle={{ width: wp("70%") }}
-                    onChangeText={val => setUsername(val)}
-                />
-                <Input
-                    placeholder="password"
-                    inputStyle={{ backgroundColor: "white" }}
-                    leftIcon={
-                        <Icon
-                            type="feather"
-                            name="lock"
-                        />
-                    }
-                    inputContainerStyle={{ backgroundColor: "white" }}
-                    containerStyle={{ width: wp("70%") }}
-                    secureTextEntry
-                    onChangeText={val => setPass(val)}
-                />
-                <Button
-                    title="Sign In"
-                    containerStyle={{ width: wp("60%"), alignSelf: "center" }}
-                    onPress={btLogin}
-                />
-            </View>
-            <Text style={style.textA}>Not have account ?
-                <Text style={style.textRegis}
-                    onPress={btToRegis}
-                >Register Now</Text>
-            </Text>
         </View>
-
-
     )
+
 }
 
 const style = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#dcdde1",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    splashScreen: {
+        flex: 1,
+        backgroundColor: "white",
         justifyContent: "center",
         alignItems: "center"
     },
